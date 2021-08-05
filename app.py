56898c0492e173ae668589818c656df113fddbbb
Harshad Patil <harsh.patil317.hp@gmail.com>
2021-08-05 10:36:07 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -19,4 +19,4 @@ def predict():
     return render_template('index.html', prediction_text = 'Employee salary should be ${}'.format(output))
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=8080)
